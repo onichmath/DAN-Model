80eb1ab350b172667da87dc12370063b3692cf8c
Matthew O <mathonich@yahoo.com>
2024-10-14 15:19:16 -0700

    New experiments

---
+++ experiments.py
@@ -0,0 +1,42 @@
+import time
+from data_loader import load_data
+from train_eval import experiment
+from plotting import save_accuracies_plot
+import torch
+from BOWmodels import SentimentDatasetBOW, NN2BOW, NN3BOW
+import optuna
+
+def SUBWORDDAN_experiment(device:torch.device):
+    pass
+
+def DAN_experiment(device:torch.device):
+    pass
+
+def DAN_experiment_optuna(device:torch.device):
+    pass
+
+def BOW_experiment_optuna(device:torch.device):
+    pass
+
+def BOW_experiment(device:torch.device):
+    train_loader, test_loader = load_data(SentimentDatasetBOW, batch_size=32)
+
+    # Train and evaluate NN2
+    start_time = time.time()
+    print('\n2 layers:')
+    nn2_train_accuracy, nn2_test_accuracy = experiment(device, NN2BOW(input_size=512, hidden_size=100), train_loader, test_loader)
+
+    # Train and evaluate NN3
+    print('\n3 layers:')
+    nn3_train_accuracy, nn3_test_accuracy = experiment(device, NN3BOW(input_size=512, hidden_size=100), train_loader, test_loader)
+    end_time = time.time()
+
+    training_accuracies = {
+            'NN2BOW': nn2_train_accuracy,
+            'NN3BOW': nn3_train_accuracy
+            }
+    testing_accuracies = {
+            'NN2BOW': nn2_test_accuracy,
+            'NN3BOW': nn3_test_accuracy
+            }
+    save_accuracies_plot(training_accuracies, testing_accuracies)
--- main.py
+++ main.py
@@ -4,30 +4,36 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from sklearn.feature_extraction.text import CountVectorizer
+from Documents.College.CSE256.hw1.CSE256_PA1_FA24.experiments import DAN_experiment
 from sentiment_data import read_sentiment_examples
 from torch.utils.data import Dataset, DataLoader
 import time
 import argparse
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
-from BOWmodels import SentimentDatasetBOW, NN2BOW, NN3BOW
-from experiments import BOW_experiment
+from experiments import BOW_experiment, BOW_experiment_optuna
 
 def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # Set up argument parser
     parser = argparse.ArgumentParser(description='Run model training based on specified model type')
     parser.add_argument('--model', type=str, required=True, help='Model type to train (e.g., BOW)')
+    parser.add_argument('--optuna', type=bool, required=False, default=False, help='Use Optuna for hyperparameter tuning')
 
     # Parse the command-line arguments
     args = parser.parse_args()
-
     # Check if the model type is "BOW"
+    if args.optuna:
+        BOW_experiment_optuna(device)
+        print("Optuna hyperparameter tuning not implemented")
+        exit()
+
     if args.model == "BOW":
-        BOW_experiment()
+        BOW_experiment(device)
     elif args.model == "DAN":
         #TODO:  Train and evaluate your DAN
+        DAN_experiment(device)
         print("DAN model not implemented yet")
-
     
     elif args.model == "SUBWORDDAN":
         #TODO:  Train and evaluate your DAN
