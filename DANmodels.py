6ed3501b29bda83b662f6353b9468a255484e74f
Matthew O <mathonich@yahoo.com>
2024-10-14 21:01:53 -0700

    Precompute indices

--- DANmodels.py
+++ DANmodels.py
@@ -18,27 +18,36 @@ class SentimentDatasetDAN(Dataset):
         self.embeddings = word_embeddings
         
         # Convert labels to PyTorch tensors
-        # self.embeddings = torch.tensor(self.embeddings, dtype=torch.float32)
         self.labels = torch.tensor(self.labels, dtype=torch.long)
+
+        self.word_indices = self._precompute_padded_word_indices()
+
+    def _precompute_padded_word_indices(self):
+        """
+        Precompute the word indices for each sentence and pad each sentence to the length of the longest sentence
+        Sets UNK indices to 1, as done in read_word_embeddings
+        Sets padding indices to 0, as done in read_word_embeddings
+        """
+        # Precompute the word indices for each sentence
+        # Pad each sentence to the length of the longest sentence
+        max_len = max(len(sent.split()) for sent in self.sentences)
+        word_indices = []
+        for sentence in self.sentences:
+            indices = [self.embeddings.word_indexer.index_of(word) for word in sentence.split()]
+            word_indices.append(indices)
+
+        # Pad sentences with 0 indices
+        word_indices = torch.full((len(word_indices), max_len), fill_value=0, dtype=torch.int)
+        # Change -1 UNK indices to 1
+        word_indices = torch.where(word_indices == -1, torch.tensor(1, dtype=torch.int), word_indices)
+        return word_indices
 
     def __len__(self):
         return len(self.examples)
 
     def __getitem__(self, idx):
         # Return sete of word indices and labels for the given index
-        # Should -1 UNK indices be changed to 1?
-        # Get the sentence at the given index
-        sentence = self.sentences[idx]
-        word_indices = torch.tensor([
-            self.embeddings.word_indexer.index_of(word) for word in sentence.split()
-        ], dtype=torch.int)
-
-        word_indices = torch.where(word_indices == -1, torch.tensor(1, dtype=torch.int), word_indices)
-
-        label = self.labels[idx]
-        print(word_indices.dtype)
-
-        return word_indices, label
+        return self.word_indices[idx], self.labels[idx]
 
 class NN2DAN(nn.Module):
     def __init__(self, word_embedding_layer, hidden_size):
