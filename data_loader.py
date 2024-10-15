d6339da95411de06525b299af360c1da5c1c72ea
Matthew O <mathonich@yahoo.com>
2024-10-14 17:48:26 -0700

    Nondefault

--- data_loader.py
+++ data_loader.py
@@ -17,7 +17,7 @@ def load_data_BOW(batch_size=32):
     print(f"BOW: Data loaded in : {elapsed_time} seconds")
     return train_loader, test_loader
 
-def load_data_DAN(batch_size, glove_dims):
+def load_data_DAN(batch_size=32, glove_dims=300):
     start_time = time.time()
     if glove_dims == 50:
         glove_file = "./data/glove.6B.50d-relativized.txt"
