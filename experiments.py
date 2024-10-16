da8d4fc16117f2cc7033a8dfaee2ee66ff9a6650
Matthew O <mathonich@yahoo.com>
2024-10-15 21:31:12 -0700

    Dan experiemnt

--- experiments.py
+++ experiments.py
@@ -1,38 +1,46 @@
 import time
+from typing import cast
 from data_loader import load_data_BOW, load_data_DAN
 from sentiment_data import WordEmbeddings
 from train_eval import experiment
-from plotting import save_accuracies_plot
+from plotting import load_accuracies, save_accuracies, save_accuracies_plot
 import torch
 from BOWmodels import NN2BOW, NN3BOW
-from DANmodels import NN2DAN, OptimalDAN
+from DANmodels import NN2DAN, OptimalDAN, SentimentDatasetDAN
+import json
 import optuna
 
 def SUBWORDDAN_experiment(device:torch.device):
     pass
 
-def DAN_experiment(device:torch.device, glove_dims:int=300):
+def DAN_experiment(device:torch.device, embed_dims:int=300):
     # device = torch.device("cpu")
-    train_loader, test_loader, word_embeddings = load_data_DAN(batch_size=32, glove_dims=glove_dims)
-    basic_danmodel = NN2DAN(word_embedding_layer=word_embeddings.get_initialized_embedding_layer(), hidden_size=100,)
+    train_loader, test_loader = load_data_DAN(batch_size=256, embed_dims=embed_dims)
+    # TODO: decaying learning rate scheduler
+    # Cast dataset to prevent errors
+    basic_danmodel = NN2DAN(train_loader.get_embedding_layer(frozen=False), hidden_size=100,)
 
     start_time = time.time()
     print('\n2 layers:')
     nn2_train_accuracy, nn2_test_accuracy = experiment(device, basic_danmodel, train_loader, test_loader)
 
-    optimal_danmodel = OptimalDAN(word_embedding_layer=word_embeddings.get_initialized_embedding_layer())
-    optdan_train_accuracy, optdan_test_accuracy = experiment(device, optimal_danmodel, train_loader, test_loader, learning_rate=0.001, weight_decay=1e-5)
+    # optimal_danmodel = OptimalDAN(word_embedding_layer=word_embeddings.get_initialized_embedding_layer(frozen=False))
+    # optdan_train_accuracy, optdan_test_accuracy = experiment(device, optimal_danmodel, train_loader, test_loader, learning_rate=0.0001, weight_decay=1e-5)
     stop_time = time.time()
+
+    # bow_train, bow_test = load_accuracies('bow_acc.json')
 
     training_accuracies = {
             'NN2DAN': nn2_train_accuracy,
-            'OPTDAN': optdan_train_accuracy 
+            # 'OPTDAN': optdan_train_accuracy 
             }
     testing_accuracies = {
             'NN2DAN': nn2_test_accuracy,
-            'OPTDAN': optdan_test_accuracy
+            # 'OPTDAN': optdan_test_accuracy
             }
-    save_accuracies_plot(training_accuracies, testing_accuracies)
+    # training_accuracies.update(bow_train)
+    # testing_accuracies.update(bow_test)
+    # save_accuracies_plot(training_accuracies, testing_accuracies)
 
 
 
@@ -63,4 +71,5 @@ def BOW_experiment(device:torch.device):
             'NN2BOW': nn2_test_accuracy,
             'NN3BOW': nn3_test_accuracy
             }
+    # save_accuracies(training_accuracies, testing_accuracies)
     save_accuracies_plot(training_accuracies, testing_accuracies)
