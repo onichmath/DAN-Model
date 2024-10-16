801ce021a0583e6ea6b9e5d034685dace5d03019
Matthew O <mathonich@yahoo.com>
2024-10-16 15:09:27 -0700

    Attempted tokenization

--- tokenizer/BPETokenizer.py
+++ tokenizer/BPETokenizer.py
@@ -29,6 +29,7 @@ class BPETokenizer():
         new_ids = []
         i = 0
         while i < len(ids):
+            # If match, replace with idx and skip next
             if i < len(ids) - 1 and (ids[i], ids[i+1]) == pair:
                 new_ids.append(idx)
                 i += 2
@@ -41,16 +42,46 @@ class BPETokenizer():
     def train_bpe(vocab_sizes=[1000, 5000, 10000, 20000, 50000, 100000]):
         # Read in labels, sentences
         text = read_sentiment_examples("./data/train.txt")
+        text = [sent.words for sent in text]
         # Convert to list of words
-        text = [ex.words for ex in text]
+        characters = []
+        for sent in text:
+            for word in sent:
+                characters.extend(list(word))
+                # word = word + "</w>"
+                # characters.extend(char for char in word)
+                characters.append("</w>")
+                # characters.append("</w>")
+        print(characters[:500])
+        print(len(characters.encode("utf-8")))
+        for sent in text:
+            # sent.words = "</w>".join(sent.words)
+            # print(sent.words.split("</w>"))
+            exit()
+        # text = [word + "</w>" for sent in text for word in sent.words]
+        text = [char for word in text for char in word]
+        print(text[:10])
         # Conver matrix of words to list of integers corresponding to words
-        all_words = " ".join([" ".join(ex) for ex in text])
-        tokens = list(map(int, all_words.encode("utf-8")))
-        print(len(tokens))
-        stats = BPETokenizer.get_stats(tokens)
-        top_pair = max(stats, key=stats.get)
-        tokens = BPETokenizer.merge(tokens, top_pair, 256)
-        print(len(tokens))
+        # all_words = " ".join([" ".join(ex) for ex in text])
+        all_words = [list("")]
+        all_chars = ""
+        print(len(all_chars))
+        # tokens = list(map(int, all_words.encode("utf-8")))
+        exit()
+        
+        ids = list(tokens)
+        num_merges = 1000 - 256
+        merges = {}
+
+        for i in range(num_merges):
+            stats = BPETokenizer.get_stats(ids)
+            top_pair = max(stats, key=stats.get)
+            idx = 256 + i
+            ids = BPETokenizer.merge(ids, top_pair, idx)
+            print(len(ids))
+            merges[top_pair] = idx
+
+        print(len(ids))
         # print(stats)
         # stats = sorted(((v,k) for k,v in stats.items()), reverse=True)
         # print(stats)
