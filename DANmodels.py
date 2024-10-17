cd8a0afaa08493547d9aa7f8e1d21517bd517afb
Matthew O <mathonich@yahoo.com>
2024-10-17 16:49:03 -0700

    N layers

--- DANmodels.py
+++ DANmodels.py
@@ -94,7 +94,7 @@ class DANModel(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def mean_ignore_padding(self, tensor):
+    def mean_ignore_padding(self, tensor: torch.Tensor):
         # Calculate the mean while ignoring 0s (padding tokens)
         mask = tensor != 0
         sum_values = torch.sum(tensor * mask, dim=1)
@@ -120,13 +120,16 @@ class NN2DAN(DANModel):
 
 class OptimalDAN(DANModel):
     # Implements the best performing DAN model in the DAN paper:  https://people.cs.umass.edu/~miyyer/pubs/2015_acl_dan.pdf
-    def __init__(self, word_embedding_layer, hidden_size=300, dropout_prob=0.3):
+    def __init__(self, word_embedding_layer, hidden_size=300, dropout_prob=0.3, n_layers=2):
         super().__init__()
         self.embedding = word_embedding_layer
-        self.fc1 = nn.Linear(self.embedding.embedding_dim, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, hidden_size)
-        self.fc4 = nn.Linear(hidden_size, 2)
+        self.embedfc = nn.Linear(self.embedding.embedding_dim, hidden_size)
+        self.sequence = nn.Sequential()
+        for i in range(n_layers):
+            self.sequence.add_module(f"fc{i}", nn.Linear(hidden_size, hidden_size))
+            self.sequence.add_module(f"relu{i}", nn.ReLU())
+            self.sequence.add_module(f"dropout{i}", nn.Dropout(dropout_prob))
+        self.outfc = nn.Linear(hidden_size, 2)
 
         self.dropout = nn.Dropout(dropout_prob)
         self.log_softmax = nn.LogSoftmax(dim=1)
@@ -136,13 +139,9 @@ class OptimalDAN(DANModel):
         x = self.dropout(x)
         x = self.mean_ignore_padding(x)
 
-        x = F.relu(self.fc1(x))
+        x = self.embedfc(x)
         x = self.dropout(x)
-        x = F.relu(self.fc2(x))
-        x = self.dropout(x)
-        x = F.relu(self.fc3(x))
-        x = self.dropout(x)
-
-        x = self.fc4(x)
+        x = self.sequence(x)
+        x = self.outfc(x)
         x = self.log_softmax(x)
         return x
