df6c87d73d6450f8cffa58ff894bcc56d934e5da
Matthew O <mathonich@yahoo.com>
2024-10-16 20:55:44 -0700

    No longer graphing

--- experiments.py
+++ experiments.py
@@ -27,16 +27,6 @@ def SUBWORDDAN_experiment(device:torch.device, visual=False):
     optimal_danmodel = OptimalDAN(word_embedding_layer=train_loader.get_embedding_layer(frozen=False))
     optdan_train_accuracy, optdan_test_accuracy = experiment(device, optimal_danmodel, train_loader, test_loader, learning_rate=0.0001, weight_decay=1e-5)
 
-    training_accuracies = {
-            'NN2BPEDAN': nn2_train_accuracy,
-            'OPTBPEDAN': optdan_train_accuracy 
-            }
-    testing_accuracies = {
-            'NN2BPEDAN': nn2_test_accuracy,
-            'OPTBPEDAN': optdan_test_accuracy
-            }
-    save_accuracies_plot(training_accuracies, testing_accuracies)
-
 def RANDOMDAN_experiment(device:torch.device):
     train_loader, test_loader = load_data_DAN(batch_size=256, use_pretrained=False, embed_dims=300)
     basic_danmodel = NN2DAN(train_loader.get_embedding_layer(frozen=False), hidden_size=100,)
@@ -60,11 +50,7 @@ def BOW_experiment(device:torch.device):
     train_loader, test_loader = load_data_BOW(batch_size=32)
 
     # Train and evaluate NN2
-    start_time = time.time()
-    print('\n2 layers:')
     nn2_train_accuracy, nn2_test_accuracy = experiment(device, NN2BOW(input_size=512, hidden_size=100), train_loader, test_loader)
 
     # Train and evaluate NN3
-    print('\n3 layers:')
     nn3_train_accuracy, nn3_test_accuracy = experiment(device, NN3BOW(input_size=512, hidden_size=100), train_loader, test_loader)
-    end_time = time.time()
