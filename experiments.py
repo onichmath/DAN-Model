3a5fac3869f6f073cba6713fb01a6d9811e48088
Matthew O <mathonich@yahoo.com>
2024-10-19 17:33:42 -0700

    Comments for experiments

--- experiments.py
+++ experiments.py
@@ -1,4 +1,3 @@
-import time
 from data_loader import load_data_BOW, load_data_DAN
 from tokenizer.BPETokenizer import BPETokenizer
 from train_eval import experiment, objective
@@ -10,40 +9,82 @@ import optuna
 
 
 def optuna_study():
+    """
+    Conducts an optuna study to find the optimal hyperparameters for the OptimalDAN model
+    """
     pruner = optuna.pruners.MedianPruner()
     study = optuna.create_study(
-            study_name="Study",
+            study_name="Optimal DAN",
             storage="sqlite:///DAN_hyperparameter_tuning.db",
             load_if_exists=True,
             direction="maximize",
             pruner=pruner)
     study.optimize(objective, n_trials=1000)
 
-def SUBWORDDAN_experiment(device:torch.device, visual=False):
+def SUBWORDDAN_experiment(device:torch.device):
+    """
+    Tests the accuracy of the two-layer and three-layer DAN models with byte pair tokenized embeddings
+    """
     tokenizer = BPETokenizer(vocab_size=10000)
     train_loader, test_loader = load_data_DAN(batch_size=256, use_pretrained=False, embed_dims=50, tokenizer=tokenizer)
     basic_danmodel = NN2DAN(train_loader.get_embedding_layer(frozen=False), hidden_size=100,)
-    nn2_train_accuracy, nn2_test_accuracy = experiment(device, basic_danmodel, train_loader, test_loader)
+    nn2_train_accuracy, nn2_test_accuracy = experiment(device, basic_danmodel, train_loader, test_loader, learning_rate=0.0001, weight_decay=1e-5)
     optimal_danmodel = OptimalDAN(word_embedding_layer=train_loader.get_embedding_layer(frozen=False))
     optdan_train_accuracy, optdan_test_accuracy = experiment(device, optimal_danmodel, train_loader, test_loader, learning_rate=0.0001, weight_decay=1e-5)
 
+    training_accuracies = {
+            "NN2DAN": nn2_train_accuracy,
+            "OptimalDAN": optdan_train_accuracy
+            }
+    testing_accuracies = {
+            "NN2DAN": nn2_test_accuracy,
+            "OptimalDAN": optdan_test_accuracy
+            }
+    save_accuracies_plot(training_accuracies, testing_accuracies)
+
 def RANDOMDAN_experiment(device:torch.device):
+    """
+    Tests the accuracy of the two-layer and three-layer DAN models with randomly initialized embeddings
+    """
     train_loader, test_loader = load_data_DAN(batch_size=256, use_pretrained=False, embed_dims=300)
     basic_danmodel = NN2DAN(train_loader.get_embedding_layer(frozen=False), hidden_size=100,)
 
-    nn2_train_accuracy, nn2_test_accuracy = experiment(device, basic_danmodel, train_loader, test_loader)
+    nn2_train_accuracy, nn2_test_accuracy = experiment(device, basic_danmodel, train_loader, test_loader, learning_rate=0.0001, weight_decay=1e-5)
     optimal_danmodel = OptimalDAN(word_embedding_layer=train_loader.get_embedding_layer(frozen=False))
     optdan_train_accuracy, optdan_test_accuracy = experiment(device, optimal_danmodel, train_loader, test_loader, learning_rate=0.0001, weight_decay=1e-5)
+    training_accuracies = {
+            "NN2DAN": nn2_train_accuracy,
+            "OptimalDAN": optdan_train_accuracy
+            }
+    testing_accuracies = {
+            "NN2DAN": nn2_test_accuracy,
+            "OptimalDAN": optdan_test_accuracy
+            }
+    save_accuracies_plot(training_accuracies, testing_accuracies)
 
 def DAN_experiment(device:torch.device, embed_dims:int=300):
+    """
+    Tests the accuracy of the two-layer and three-layer DAN models with pretrained embeddings
+    """
     train_loader, test_loader = load_data_DAN(batch_size=256, embed_dims=embed_dims, use_pretrained=True)
     basic_danmodel = NN2DAN(train_loader.get_embedding_layer(frozen=False), hidden_size=100,)
 
     print('\n2 layers:')
-    nn2_train_accuracy, nn2_test_accuracy = experiment(device, basic_danmodel, train_loader, test_loader)
+    nn2_train_accuracy, nn2_test_accuracy = experiment(device, basic_danmodel, train_loader, test_loader, learning_rate=0.0001, weight_decay=1e-5)
 
     optimal_danmodel = OptimalDAN(word_embedding_layer=train_loader.get_embedding_layer(frozen=False))
     optdan_train_accuracy, optdan_test_accuracy = experiment(device, optimal_danmodel, train_loader, test_loader, learning_rate=0.0001, weight_decay=1e-5)
+
+    training_accuracies = {
+            "NN2DAN": nn2_train_accuracy,
+            "OptimalDAN": optdan_train_accuracy
+            }
+    testing_accuracies = {
+            "NN2DAN": nn2_test_accuracy,
+            "OptimalDAN": optdan_test_accuracy
+            }
+    save_accuracies_plot(training_accuracies, testing_accuracies)
+
 
 def BOW_experiment(device:torch.device):
     # Tests the accuracy of the two-layer and three-layer BOW models
